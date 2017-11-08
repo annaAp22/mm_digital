735accf85ef137fce0dc971f1f20839ab7e895cb
Anna <apuhtina.mmdigital@gmail.com>
2017-11-08 22:39:30 +0300

    08.11

--- cases/fit2u/index.php
+++ cases/fit2u/index.php
@@ -1,158 +1,169 @@
 <!DOCTYPE html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Fit2U</title>
-    <link rel="icon" sizes="16x16" href="/assets/images/favicon.png?v=1">
-    <link rel="stylesheet" href="/assets/css/med_serdce.min.css?v=20170404001">
-    <link rel="stylesheet" href="/assets/css/fit2u.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Fit2U</title>
+  <link rel="icon" sizes="16x16" href="/assets/images/favicon.png?v=1">
+  <link rel="stylesheet" href="/assets/css/med_serdce.min.css?v=20170404001">
+  <link rel="stylesheet" href="/assets/css/fit2u.min.css">
 
 </head>
 <body>
 
-<!-- Fixed header Start -->
-<header class="wrapper row">
+  <!-- Fixed header Start -->
+  <header class="wrapper row">
     <!-- Menu button Start -->
     <div class="menu-sidebar column" id="nav-open-btn">
-        <div></div>
-        <div></div>
-        <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
     <!-- Menu button End -->
 
     <div class="scale logo column">
-        <a href="/"><i class="sprite_ms sprite_ms-logo"></i></a>
+      <a href="/"><i class="sprite_ms sprite_ms-logo"></i></a>
     </div>
 
     <div class="contacts pull-right">
-        <div class="phone inline-block-middle">
-            <i class="sprite_ms sprite_ms-phone_1"></i>
-            <div class="inline-block-middle proxima">
-                +7 ( 926 ) <span class="proxima-extrabold">422 06 18</span>
-            </div>
-            <br>
-            <a class="email-link proxima" href="mailto:a.ceo@mm-media.ru">a.ceo@mm-media.ru</a>
+      <div class="phone inline-block-middle">
+        <i class="sprite_ms sprite_ms-phone_1"></i>
+        <div class="inline-block-middle proxima">
+          +7 ( 926 ) <span class="proxima-extrabold">422 06 18</span>
         </div>
-        <div class="btn-wrap inline-block-middle">
-            <button class="btn btn-1 proxima-extrabold ajax-modal" data-type="request">Сделать заказ</button>
-        </div>
+        <br>
+        <a class="email-link proxima" href="mailto:a.ceo@mm-media.ru">a.ceo@mm-media.ru</a>
+      </div>
+      <div class="btn-wrap inline-block-middle">
+        <button class="btn btn-1 proxima-extrabold ajax-modal" data-type="request">Сделать заказ</button>
+      </div>
     </div>
 
-</header>
-<!-- Fixed header End -->
+  </header>
+  <!-- Fixed header End -->
 
-<!-- Sidebar navigation Start -->
-<nav id="nav">
+  <!-- Sidebar navigation Start -->
+  <nav id="nav">
     <div class="tac logo">
-        <img src="/assets/images/aside_logo-min.png" alt="mm-media">
+      <img src="/assets/images/aside_logo-min.png" alt="mm-media">
     </div>
     <ul class="navigation proxima-bold">
-        <!-- <li><a href="#"><span>Кто мы</span></a></li>
-        <li><a href="#"><span>Услуги</span></a></li> -->
-        <li>
-            <a href="/"><span>Главная</span></a>
-        </li>
-        <li>
-            <a><span>Мед-Сердце</span></a>
-        </li>
-        <li>
-            <a href="/cases/litstile"><span>Литстиль</span></a>
-        </li>
-        <li>
-            <a href="/cases/estetclinic"><span>Эстет Клиник</span></a>
-        </li>
-        <li>
-            <a href="/cases/beurer"><span>Beurer</span></a>
-        </li>
-        <!-- <li><a href="#contacts"><span>Контакты</span></a></li> -->
+      <!-- <li><a href="#"><span>Кто мы</span></a></li>
+      <li><a href="#"><span>Услуги</span></a></li> -->
+      <li>
+        <a href="/"><span>Главная</span></a>
+      </li>
+      <li>
+        <a><span>Мед-Сердце</span></a>
+      </li>
+      <li>
+        <a href="/cases/litstile"><span>Литстиль</span></a>
+      </li>
+      <li>
+        <a href="/cases/estetclinic"><span>Эстет Клиник</span></a>
+      </li>
+      <li>
+        <a href="/cases/beurer"><span>Beurer</span></a>
+      </li>
+      <!-- <li><a href="#contacts"><span>Контакты</span></a></li> -->
     </ul>
     <a class="close-btn" id="nav-close-btn" href="#top">&#10005;</a>
-</nav>
-<!-- Sidebar navigation End -->
-<main class="main-page">
+  </nav>
+  <!-- Sidebar navigation End -->
+  <main class="main-page">
     <!-- Content Start -->
     <section class="content blured" id="main">
-        <div class="header">
-            <div class="wrapper row"></div>
-        </div>
+      <div class="header">
+        <div class="wrapper row"></div>
+      </div>
     </section>
     <article class="unit unit--00">
-        <h2 class="visualy-hidden">Ребрендинг и новый дизайн магазина фитнес одежды</h2>
-        <div class="container">
-            <div class="unit__block unit__block--1">&nbsp;
-                <img class="unit__img00" src="/assets/images/cases/fit2u/screen.png" alt="laptop">
-            </div>
-            <div class="unit__block unit__block--2">
-                <a href="http://fit2u.ru/" class="unit__link proxima-extrabold"><i
-                            class="scale sprite_ms sprite_ms-link_1"></i><span>fit2u.ru</span></a>
-                <p class="proxima unit__descr">Ребрендинг и новый дизайн магазина фитнес одежды</p>
-                <p class="unit__attainment proxima-extrabold">
-                    Создали яркий и узнаваемый интернет магазин
-                </p>
-                <div class="unit__task">
-                    <div class="unit__task-corner"></div>
-                    <span class="proxima-bold">Задача:</span>
-                    <ul class="proxima unit__task-lst">
-                        <li><i class="trigon ">&#9650;</i> Переработать устаревший дизайн интернет магазина спортивной
-                            одежды
-                        </li>
-                        <li><i class="trigon trigon--90">&#9650;</i>Разработать удобный интерфейс</li>
-                        <li><i class="trigon trigon--45">&#9650;</i>Разработать узнаваемый branding</li>
-                        <li><i class="trigon trigon--m45">&#9650;</i>Ускорить работу сайта</li>
-                        <li><i class="trigon trigon--90">&#9650;</i>Сделать упаковку всех бизнес-преимуществ</li>
-                    </ul>
-                </div>
-            </div>
+      <h2 class="visualy-hidden">Ребрендинг и новый дизайн магазина фитнес одежды</h2>
+      <div class="container">
+        <div class="unit__block unit__block--1">
+          <img class="unit__img00" src="/assets/images/cases/fit2u/screen.png" alt="laptop">
+        </div>
+        <div class="unit__block unit__block--2">
+          <a href="http://fit2u.ru/" class="unit__link proxima-extrabold"><i
+                class="scale sprite_ms sprite_ms-link_1"></i><span>fit2u.ru</span></a>
+          <p class="proxima unit__descr">Ребрендинг и новый дизайн магазина фитнес одежды</p>
+          <p class="unit__attainment proxima-extrabold">
+            Создали яркий и узнаваемый интернет магазин
+          </p>
+          <div class="unit__task">
+            <div class="unit__task-corner"></div>
+            <span class="proxima-bold">Задача:</span>
+            <ul class="proxima unit__task-lst">
+              <li><i class="trigon ">&#9650;</i> Переработать устаревший дизайн интернет магазина спортивной
+                одежды
+              </li>
+              <li><i class="trigon trigon--90">&#9650;</i>Разработать удобный интерфейс</li>
+              <li><i class="trigon trigon--45">&#9650;</i>Разработать узнаваемый branding</li>
+              <li><i class="trigon trigon--m45">&#9650;</i>Ускорить работу сайта</li>
+              <li><i class="trigon trigon--90">&#9650;</i>Сделать упаковку всех бизнес-преимуществ</li>
+            </ul>
+          </div>
+        </div>
     </article>
     <article class="unit unit--001">
-        <div class="container">
-            <div class="unit__block">&nbsp;
-                <h3 class="proxima-extrabold">Создание логотипа и общей стилистики сайта</h3>
-                <p class="proxima unit__idea"><span class="proxima-bold">Идея:</span> лого серьезного интернет-магазина
-                    одежды. Цифра два огибает букву U, что символизирует
-                    дословно то, что впору клиенту. Как цифра огибает и подстраивается под букву U, так одежда магазина
-                    - это
-                    именно то, что подойдет клиенту</p>
+      <div class="container">
+        <div class="unit__block">&nbsp;
+          <h3 class="proxima-extrabold ">Создание логотипа и общей стилистики сайта</h3>
+          <p class="proxima unit__idea"><span class="proxima-bold">Идея:</span> лого серьезного интернет-магазина
+            одежды. Цифра два огибает букву U, что символизирует
+            дословно то, что впору клиенту. Как цифра огибает и подстраивается под букву U, так одежда магазина
+            - это
+            именно то, что подойдет клиенту</p>
 
-                <img class="unit__img" src="/assets/images/cases/fit2u/logo2.png" alt="логотип">
+          <img class="unit__img" src="/assets/images/cases/fit2u/logo2.png" alt="логотип">
 
-                <h3 class="proxima-extrabold">Палитра на сайте</h3>
-                <div class="palette">
-                    <div class="palette-item palette-item--yellow proxima-semibold">#fcd82f</div>
-                    <div class="palette-item palette-item--green proxima-semibold">#35b056</div>
-                    <div class="palette-item palette-item--black proxima-semibold">#1e0f00</div>
-                    <div class="palette-item palette-item--grey proxima-semibold">#f4f4f4</div>
-                    <div class="palette-item palette-item--white proxima-semibold">#ffffff</div>
+          <h3 class="proxima-extrabold">Палитра на сайте</h3>
+          <div class="palette">
+            <div class="palette-item palette-item--yellow proxima-semibold">#fcd82f</div>
+            <div class="palette-item palette-item--green proxima-semibold">#35b056</div>
+            <div class="palette-item palette-item--black proxima-semibold">#1e0f00</div>
+            <div class="palette-item palette-item--grey proxima-semibold">#f4f4f4</div>
+            <div class="palette-item palette-item--white proxima-semibold">#ffffff</div>
 
-                </div>
-            </div>
-            <div class="unit__block">
-                <h2 class="unit__blk-title proxima-extrabold">Работа над брэндом</h2>
-
-            </div>
+          </div>
         </div>
-        <div class="unit__brand-full">
-            <div class="unit__blk-content">
-                <p class="proxima-semibold"><span class="proxima-extrabold">Fit2U</span> - это интернет-магазин модной
-                    фитнес одежды для
-                    женщин и мужчина. Европейский дизайн и качественная ткань.
-                </p>
-                <p class="proxima-semibold">Заказчик очень хотел подчеркнуть яркий и позитивный настрой компании,
-                    дружелюбный сервис и, в то же время,
-                    серьезность организации. Мы помогли в реализации узнаваемости компании</p>
-                <button class="btn btn-fit proxima-extrabold ajax-modal" data-type="request">
-                    Хочу заказать ребрендинг
-                </button>
-            </div>
+        <div class="unit__block">
+          <h2 class="unit__blk-title proxima-extrabold">Работа над брэндом</h2>
+
         </div>
+      </div>
+      <div class="unit__brand-full">
+        <div class="unit__blk-content">
+          <p class="proxima-semibold"><span class="proxima-extrabold">Fit2U</span> - это интернет-магазин модной
+            фитнес одежды для
+            женщин и мужчина. Европейский дизайн и качественная ткань.
+          </p>
+          <p class="proxima-semibold">Заказчик очень хотел подчеркнуть яркий и позитивный настрой компании,
+            дружелюбный сервис и, в то же время,
+            серьезность организации. Мы помогли в реализации узнаваемости компании</p>
+          <button class="btn btn-fit proxima-extrabold ajax-modal" data-type="request">
+            Хочу заказать ребрендинг
+          </button>
+        </div>
+      </div>
     </article>
     <article class="unit unit--01">
-
+      <div class="container">
+        <div class="unit__title">
+          <p class="unit__title-bck proxima-semibold">01</p>
+          <h2 class="proxima-extrabold">Главная страница</h2>
+        </div>
+        <p class="unit_descr proxima-semibold">Гибридная структура. Главная страница представляет собой как обычный сайт, так и landing page. Яркие образы, логические акценты, правильно поданные смыслы</p>
+        <div class="unit__title proxima-semibold">
+          <p class="unit__title-bck unit__title-bck--2 ">FONT</p>
+          <p class="unit__txt"><span class="proxima-extrabold">Шрифт:</span><br>&nbsp;
+            <br>Proxima Nova </p>
+        </div>
+      </div>
     </article>
     <!-- Content End -->
-</main>
-<div class="column">
+  </main>
+  <div class="column">
 
     <!-- Block 17 Contacts Start -->
     <!-- <footer class="block block-17 block-contacts row">
